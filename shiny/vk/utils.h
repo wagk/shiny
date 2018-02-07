1b29872f9e52eb732f62e4d0cc6b58ec30553edc
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2018-02-08 00:19:51 +0800

    added vk/utils.h to store the collect template

---
+++ shiny/vk/utils.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+
+namespace shiny::vk {
+
+    template<typename R, typename F, typename H>
+    std::vector<R> collect(F func, const H& handle) 
+    {
+        uint32_t count = 0;
+        func(handle, &count, nullptr);
+
+        std::vector<R> vec(count);
+        func(handle, &count, vec.data());
+
+        return vec;
+    }
+
+    template<typename R, typename F>
+    std::vector<R> collect(F func) 
+    {
+        uint32_t count = 0;
+        func(&count, nullptr);
+
+        std::vector<R> vec(count);
+        func(&count, vec.data());
+
+        return vec;
+    }
+
+}
