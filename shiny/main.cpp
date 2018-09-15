4ac314c9e7f3ae1b091c02dc691f990fa21ec84d
Jason Hwang <crasonghwang@gmail.com>
2018-09-14 20:49:06 -0700

    Removed OFBX since I'm no longer using it. Added stdafx for centralized common includes.
    Starting to turn the engine into a windows application in preparation for receiving inputs.

--- shiny/main.cpp
+++ shiny/main.cpp
@@ -1,24 +1,30 @@
-#define GLFW_INCLUDE_VULKAN
-#include <FreeImage.h>
-#include <GLFW/glfw3.h>
+#include "stdafx.h"
 
 #include <iostream>
 #include <stdexcept>
 
 #include <graphics/renderer.h>
-//#include <renderer.h>
-//#include <vk/instance.h>
-//#include <window.h>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define MAX_LOADSTRING 100
+
+HINSTANCE hInst;  // current instance
+WCHAR     szTitle[MAX_LOADSTRING];
+WCHAR     szWindowClass[MAX_LOADSTRING];
+
+// Forward declarations of functions included in this code module:
+// ATOM    MyRegisterClass(HINSTANCE hInstance);
+// BOOL    InitInstance(HINSTANCE, int);
+// LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// This is the old main. Keeping this as a fallback until I get the rest setup right
 int
 main()
 {
     shiny::graphics::renderer renderer;
 
     try {
-        // while (shiny::renderer::singleton().glfw_window().close_window() == false) {
-        //    shiny::renderer::singleton().glfw_window().poll_events();
-        //}
         renderer.run();
     } catch (const std::runtime_error& e) {
         std::cerr << e.what() << std::endl;
