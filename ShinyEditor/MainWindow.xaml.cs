f59b58151dd20b8e8a50c371d6905d401565e526
crasong <crasonghwang@gmail.com>
2025-01-28 01:36:40 -0800

    A new chapter begins. Added 2 new subprojects: a Static Library called "Engine", and a WPF .NET application called "ShinyEditor". Added some stubs for a Project Browser Dialog. Following a tutorial as I learn WPF

---
+++ ShinyEditor/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using ShinyEditor.GameProject;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShinyEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded += OnMainWindowLoaded;
+            OpenProjectBrowserDialog();
+        }
+
+        private void OpenProjectBrowserDialog()
+        {
+            var projectBrowser = new ProjectBrowserDialog();
+            if (projectBrowser.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
